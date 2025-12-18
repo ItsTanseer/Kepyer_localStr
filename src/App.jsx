627f7f1f56699d4713ef6a4516e504c9a2e6910b
Tanseer <tanseer.ahmad.2007@gmail.com>
2025-12-18 17:54:21 +0530

    bg fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
+  import { useState } from 'react'
 
-import './App.css'
-import Navbar from './Components/Navbar'
-import Manager from './Components/Manager'
+  import './App.css'
+  import Navbar from './Components/Navbar'
+  import Manager from './Components/Manager'
 
-function App() {
-  const [count, setCount] = useState(0)
+  function App() {
+    const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      <video className='-z-10  fixed object-cover h-screen w-screen inset-0' autoPlay loop  muted src='./public/bg-video.mp4'>
-        
-      </video>
-    
-    <Navbar/>
-     <Manager/>
-    </div>
-  )
-}
+    return (
+      <div>
+        <video className='-z-10  fixed object-cover h-screen w-screen inset-0' autoPlay loop  muted src='/bg-video.mp4'>
+          
+        </video>
+      
+      <Navbar/>
+      <Manager/>
+      </div>
+    )
+  }
 
-export default App
+  export default App
